dec7f9c88fe2906c1995f689bade4cdbac3b18f9
Vdovenko Sergey <39949940+DeFauste@users.noreply.github.com>
2024-07-14 19:56:16 +0300

    add: save: water test

---
+++ Assets/DropSpawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class DropSpawner : MonoBehaviour
+{
+    public GameObject DropPrefab;
+    [SerializeField] Transform point;
+
+    private float cooldown = 0;
+    private Camera mainCamera;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            cooldown -= Time.deltaTime;
+            while (cooldown < 0)
+            {
+                cooldown += 0.01f;
+                Instantiate(DropPrefab, (Vector2)point.position + Random.insideUnitCircle*.2f, Quaternion.identity);
+            }
+        }
+    }
+}
