f6fc81ff64b36dcf8b977e3958f48b984abe3993
Vdovenko Sergey <39949940+DeFauste@users.noreply.github.com>
2024-07-15 17:17:41 +0300

    save: load manager sketch

--- Assets/PrefabsUI/LevelSaver.cs
+++ Assets/PrefabsUI/LevelSaver.cs
@@ -1,6 +1,8 @@
+using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 
 public struct DataLvl
 {
@@ -8,9 +10,9 @@ public struct DataLvl
     public bool lockLvl;
     public bool doneLvl;
 }
-public class LevelSaver
+public class LevelSaver: MonoBehaviour
 {
-    public List<DataLvl> dataLvlList;
+    public List<DataLvl> dataLvlList = new List<DataLvl>();
 
     private void Start()
     {
@@ -21,8 +23,17 @@ public class LevelSaver
         dataLvlList.Add(new DataLvl { numericLVl = 1, lockLvl = true, doneLvl = false} );
         dataLvlList.Add(new DataLvl { numericLVl = 2, lockLvl = true, doneLvl = false} );
         dataLvlList.Add(new DataLvl { numericLVl = 3, lockLvl = true, doneLvl = false} );
-        //string json = JsonConvert.SerializeObject(dataLvlList, Formatting.Indented);
-        //Debug.Log( json );
+        string json = JsonConvert.SerializeObject( dataLvlList, Formatting.Indented );
+        var filePath = Path.Combine(Directory.GetCurrentDirectory(), "Assets\\Resources", "Levels.json");
+        if (File.Exists(filePath))
+        {
+            File.WriteAllText(filePath, json);
+            Debug.Log(filePath);
+        }
+        else
+        {
+            Debug.Log(filePath);
+        }
     }
 
     public void Load() 
