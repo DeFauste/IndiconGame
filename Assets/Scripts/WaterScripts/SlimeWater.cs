4e3f3476249459694915cc4e2e979ea8c4961037
Vdovenko Sergey <39949940+DeFauste@users.noreply.github.com>
2024-07-16 23:00:33 +0300

    fix: water show after pump

--- Assets/Scripts/WaterScripts/SlimeWater.cs
+++ Assets/Scripts/WaterScripts/SlimeWater.cs
@@ -18,6 +18,10 @@ public class SlimeWater : WaterProperty
             transform.position = new Vector3(transform.position.x, transform.position.y - 0.03f*forcePump * Time.deltaTime, transform.position.z);
             transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y - 0.01f * forcePump * Time.deltaTime, transform.localScale.z);
             pumpSize = 1;
+        } else
+        {
+            transform.localScale = new Vector3(transform.localScale.x, 0 , transform.localScale.z);
+            gameObject.SetActive(false);
         }
 
         return pumpSize;
