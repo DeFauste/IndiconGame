3f80d1d63e90c6e0ca2c3eb7a27aebfd0688f9c6
Igor Galiakberov <galiakberovigor@gmail.com>
2024-07-18 15:39:41 +0300

    Level2 complete
    Level2 complete

--- Assets/Scripts/Wind.cs
+++ Assets/Scripts/Wind.cs
@@ -11,25 +11,32 @@ public class Wind : MonoBehaviour
     [SerializeField] private string tag;
     [SerializeField] protected Vector2 velocity = Vector2.zero;
     private GameObject gameObject;
-    
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.tag.Equals(tag))
         {
             Debug.Log("yes");
+
             gameObject = collision.gameObject;
-            Rigidbody2D _rigidbody2D = collision.gameObject.GetComponent<Rigidbody2D>();
-            _rigidbody2D.AddForce(velocity, ForceMode2D.Force);                
+
+            float influence = gameObject.transform.localScale.x;
+
+            if (influence < 1.3)
+            {
+                Rigidbody2D _rigidbody2D = collision.gameObject.GetComponent<Rigidbody2D>();
+                _rigidbody2D.AddForce(velocity, ForceMode2D.Force);
+            }
         }
     }
 }
