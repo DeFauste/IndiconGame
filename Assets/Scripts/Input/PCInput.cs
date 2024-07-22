f7b01e327717ba50f1ce7ecaf3908fe4a4ab75a5
Vdovenko Sergey <39949940+DeFauste@users.noreply.github.com>
2024-07-22 12:40:08 +0300

    add: PCInput

--- Assets/Scripts/Input/PCInput.cs
+++ Assets/Scripts/Input/PCInput.cs
@@ -1,20 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
+using Zenject;
 
 namespace Assets.Scripts.Input
 {
     public class PCInput : IGamePlayInput
     {
+        PlayerInput _inputActions;
         public event Action OnJump;
         public event Action OnInteract;
 
-        public Vector2 HorizontalDirection()
+        [Inject]
+        public PCInput(PlayerInput inputActions)
         {
-            throw new NotImplementedException();
+            _inputActions = inputActions;
+            _inputActions.PCGameplay.Jump.performed += _ => Jump();
+            _inputActions.PCGameplay.F.performed += _ => OnInteracrt();
+        }
+        private void Jump()
+        {
+            OnJump?.Invoke();
+        }
+
+        private void OnInteracrt()
+        {
+            OnInteract?.Invoke(); 
+        }
+
+        public Vector2 HorizontalDirection() => _inputActions.PCGameplay.Moveble.ReadValue<Vector2>();
+
+        ~PCInput()
+        {
+            _inputActions.PCGameplay.Jump.performed -= _ => Jump();
+            _inputActions.PCGameplay.F.performed -= _ => OnInteracrt();
         }
     }
 }
