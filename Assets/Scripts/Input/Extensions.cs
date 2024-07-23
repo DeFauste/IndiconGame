7a4f97fef2a24b6bd6c26b47356c683ccca6702c
Vdovenko Sergey <39949940+DeFauste@users.noreply.github.com>
2024-07-23 14:03:13 +0300

    add: player controller jump sketch

---
+++ Assets/Scripts/Input/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Input
+{
+    public static class Extensions
+    {
+        public static bool IsMyTouchingLayers(this Collider2D collider2D, LayerMask layerMask)
+        {
+            if (collider2D != null) { return collider2D.IsTouchingLayers(layerMask); }
+            return false;
+        }
+    }
+}
--- Assets/Scripts/Input/PlayerController.cs
+++ Assets/Scripts/Input/PlayerController.cs
@@ -1,13 +1,28 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using Zenject;
 
 namespace Assets.Scripts.Input
 {
+    [RequireComponent(typeof(Rigidbody2D))]
     public class PlayerController : MonoBehaviour
     {
-        [SerializeField] 
-
+        private Rigidbody2D _rb;
         private IGamePlayInput _gamePlayInput;
+
+        #region MOVE
+        [SerializeField] private float SpeedMove = 10f;
+        #endregion
+
+        #region JUMP
+        [SerializeField] private float JumpForce = 10f;
+        [SerializeField] private LayerMask ObstacleLayers;
+        [SerializeField] Collider2D WallCheck;
+        [SerializeField] Collider2D GroundCheck;
+        [SerializeField] private bool _isGrounded;
+        [SerializeField] private bool _isWall;
+        #endregion
 
         [Inject]
         public void Construct(IGamePlayInput gamePlayInput)
@@ -18,18 +33,48 @@ namespace Assets.Scripts.Input
 
         void Start()
         {
+            InitProperty();
             _gamePlayInput.OnJump += Jump;
             _gamePlayInput.OnInteract += Interact;
-
         }
 
+        private void InitProperty()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+
+        }
+        private void FixedUpdate()
+        {
+            Move();
+            CheckObstacle();
+        }
+
+        private void CheckObstacle() 
+        {
+            _isWall = WallCheck.IsMyTouchingLayers(ObstacleLayers);
+            _isGrounded = GroundCheck.IsMyTouchingLayers(ObstacleLayers);
+        }
+        
         private void Move()
         {
-            
+            _rb.velocity = new Vector2(SpeedMove * _gamePlayInput.HorizontalDirection().x, _rb.velocity.y);
+            if (_rb.velocity.x != 0)
+            {
+                gameObject.transform.localScale = _rb.velocity.x > 0 ? new Vector3(-1, 1, 1) : new Vector3(1, 1, 1);
+            }
         }
         private void Jump()
         {
-            Debug.Log("Jump");
+            float directionJump = _rb.velocity.x;
+            if (_isWall && !_isGrounded)
+            {
+                directionJump = -gameObject.transform.localScale.x * SpeedMove;
+                Debug.Log($"Direction Jump: {directionJump}");
+            }
+            if (_isWall || _isGrounded)
+            {
+                _rb.velocity = new Vector2(directionJump, JumpForce);
+            }
         }
 
         private void Interact()
@@ -41,5 +86,6 @@ namespace Assets.Scripts.Input
         {
             _gamePlayInput.OnJump -= Jump;
         }
+
     }
 }
