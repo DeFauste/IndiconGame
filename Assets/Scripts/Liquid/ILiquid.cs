38baf7908909e290e0ca4b7d4b8f01e9ab419bbc
Vdovenko Sergey <39949940+DeFauste@users.noreply.github.com>
2024-07-23 18:44:22 +0300

    Add: ILiquid

---
+++ Assets/Scripts/Liquid/ILiquid.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Liquid
+{
+    public interface ILiquid
+    {
+        // void Pump(int forcePump);
+        // float Fresh(int forcePump, float V);
+        // bool SetPropertyWater(EWaterProperty type);
+        // float GetSquare();
+        EWaterProperty Property { get; set; }
+        float SubSquare();
+        float GetSquare();
+        void ResizeSquare();
+        float Pump(int forcePump);
+        float Fresh(int forcePump, float square);
+    }
+}
--- Assets/Scripts/Player/PlayerPumpLiquid.cs
+++ Assets/Scripts/Player/PlayerPumpLiquid.cs
@@ -1,14 +1,16 @@
 ﻿using Assets.Scripts.Input;
 using UnityEngine;
 using Zenject;
+using Assets.Scripts.Liquid;
 
 namespace Assets.Scripts.Player
 {
     public class PlayerPumpLiquid : MonoBehaviour
     {
-        [SerializeField] LayerMask layerMask;
+        [SerializeField] LayerMask LiquidLayerMask;
         private IGamePlayInput _gamePlayInput;
         private IPlayerProperty _playerProperty;
+        ContactFilter2D contactFilter = new ContactFilter2D();
         private bool _isInteract = false;
         [SerializeField] private Collider2D _playerCollider;
 
@@ -22,6 +24,8 @@ namespace Assets.Scripts.Player
         {
             _playerProperty = GetComponent<IPlayerProperty>();
             _gamePlayInput.OnInteract += SetInteract;
+            contactFilter.SetLayerMask(LiquidLayerMask);
+            contactFilter.useTriggers = true;
         }
 
 
@@ -42,10 +46,16 @@ namespace Assets.Scripts.Player
         private void LiquidPump()
         {
             Collider2D[] collider2Ds = new Collider2D[1];
-            ContactFilter2D contactFilter = new ContactFilter2D();
-            contactFilter.layerMask = layerMask;
             int count = _playerCollider.OverlapCollider(contactFilter, collider2Ds);
-            if(count > 0) Debug.Log("Pump");
+            if (count > 0)
+            {
+                Debug.Log("Pump");
+                ILiquid i = collider2Ds[0].gameObject.GetComponent<ILiquid>();
+                if (i != null)
+                {
+
+                }
+            }
 
         }
 
