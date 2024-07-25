a90ce1a8bf4ea0f17b73312db7fcfda09eb5217f
Vdovenko Sergey <39949940+DeFauste@users.noreply.github.com>
2024-07-25 13:24:19 +0300

    save: mobile input

--- Assets/Scripts/Input/MobileInput.cs
+++ Assets/Scripts/Input/MobileInput.cs
@@ -3,15 +3,34 @@ using UnityEngine;
 
 namespace Assets.Scripts.Input
 {
-    public class MobileInput : IGamePlayInput
+    public class MobileInput : MonoBehaviour, IGamePlayInput
     {
         public event Action OnJump;
         public event Action<bool> OnInteract;
 
+        private string _jumpButton = "Jump";
+        private string _squeezeButton = "Squeeze";
+       
         public Vector2 HorizontalDirection()
         {
             float x = SimpleInput.GetAxis("Horizontal");
             return new Vector2(x, 0);
+        }
+
+        private void Update()
+        {
+            if (SimpleInput.GetButtonDown(_jumpButton))
+            {
+                OnJump?.Invoke();
+            }
+            if (SimpleInput.GetButtonDown(_squeezeButton))
+            {
+                OnInteract?.Invoke(true);
+            }
+            else if (SimpleInput.GetButtonUp(_squeezeButton))
+            {
+                OnInteract?.Invoke(true);
+            }
         }
     }
 }
--- Assets/Scripts/Installers/InputInstaller.cs
+++ Assets/Scripts/Installers/InputInstaller.cs
@@ -8,18 +8,20 @@ namespace Assets.Scripts.Installers
     {
         PlayerInput playerInput;
         IGamePlayInput gamePlayInput;
+        [SerializeField] private MobileInput mobileInput;
         public override void InstallBindings()
         {
             Activate();
             Container.Bind<PlayerInput>().FromInstance(playerInput).AsSingle();
-            if(SystemInfo.deviceType == DeviceType.Desktop)
-            {
-                Container.Bind<IGamePlayInput>().To<PCInput>().AsSingle();
-            }
-            else if(SystemInfo.deviceType == DeviceType.Handheld)
-            {
-                Container.Bind<IGamePlayInput>().To<MobileInput>().AsSingle();
-            }
+            //if(SystemInfo.deviceType == DeviceType.Desktop)
+            //{
+            //    Container.Bind<IGamePlayInput>().To<PCInput>().AsSingle();
+            //}
+            //else if(SystemInfo.deviceType == DeviceType.Handheld)
+            //{
+            //    Container.Bind<IGamePlayInput>().To<MobileInput>().AsSingle();
+            //}
+            Container.Bind<IGamePlayInput>().FromInstance(mobileInput).AsSingle();
         }
         private void Activate()
         {
