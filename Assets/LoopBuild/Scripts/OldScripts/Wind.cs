a2b28692ee71ea8057bb3e9bc2cc10a9494834ec
Vdovenko Sergey <39949940+DeFauste@users.noreply.github.com>
2024-08-26 21:13:38 +0300

    add: wind

--- Assets/LoopBuild/Scripts/OldScripts/Wind.cs
+++ Assets/LoopBuild/Scripts/OldScripts/Wind.cs
@@ -2,25 +2,33 @@ using UnityEngine;
 
 public class Wind : MonoBehaviour
 {
-    // Start is called before the first frame update
-
-    [SerializeField] private string tag;
-    [SerializeField] protected Vector2 velocity = Vector2.zero;
-    private GameObject gameObject;
-
+    [SerializeField] private Vector2 velocity = Vector2.zero;
+    [SerializeField] private float scalePush = 1.3f;
+    Collider2D _collision;
+    Rigidbody2D _rb;
+    public bool OnOff {  get; set; } = true;
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.gameObject.tag.Equals(tag))
+       
+
+        if (_collision == null && _collision != collision)
         {
-            gameObject = collision.gameObject;
+            _rb = collision.gameObject.GetComponent<Rigidbody2D>();
+            
+        }
+        if(_rb != null)
+        {
+            float influence = collision.gameObject.transform.localScale.x;
 
-            float influence = gameObject.transform.localScale.x;
-
-            if (influence < 1.3)
+            if (influence < scalePush)
             {
-                Rigidbody2D _rigidbody2D = collision.gameObject.GetComponent<Rigidbody2D>();
-                _rigidbody2D.AddForce(velocity, ForceMode2D.Force);
+                _rb.AddForce(velocity, ForceMode2D.Force);
             }
         }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        _collision = null;
+        _rb = null;
     }
 }
